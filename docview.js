aa62c6afe546bd23a7541019fed03e6ab5d3d3a4
James-Livesey <jamesliveg@gmail.com>
2022-04-22 16:35:49 +0100

    Update URL in copyright header

--- docview.js
+++ docview.js
@@ -3,7 +3,7 @@
 
     Copyright (C) LiveG. All Rights Reserved.
 
-    https://liveg.tech
+    https://docs.liveg.tech
     Licensed by the LiveG Open-Source Licence, which can be found at LICENCE.md.
 */
 
